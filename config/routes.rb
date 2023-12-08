a396280dcebca861009de7405baaedf219985d6b
akshay-022 <62378087+akshay-022@users.noreply.github.com>
2023-12-07 19:24:54 -0500

    Before starting routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,17 @@ Rails.application.routes.draw do
   authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web => '/sidekiq'
   end
-
+  resources :organizations do 
+    resources :topics do
+      resources :posts, except: [:edit, :update] do
+        resources :comments, only: [:create, :destroy]
+        member do
+          post :repost
+        end
+      end
+    end
+  end
+  
   resources :posts, except: [:edit, :update] do
     resources :comments, only: [:create, :destroy]
     member do
