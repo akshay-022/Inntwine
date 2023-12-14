8e27ed79993968fcf48446e27291b826c1e31307
akshay-022 <62378087+akshay-022@users.noreply.github.com>
2023-12-14 14:06:17 -0500

    Messaging fixed

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -7,9 +7,9 @@ class ConversationsController < ApplicationController
     end
     def create  
         if Conversation.between(params[:sender_id], params[:recipient_id]).present? 
-        @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
+            @conversation = Conversation.find_by(sender_id: params[:sender_id], recipient_id: params[:recipient_id])
         else
-        @conversation = Conversation.create!(conversation_params)
+            @conversation = Conversation.create!(conversation_params)
         end
         redirect_to conversation_messages_path(@conversation)
     end
