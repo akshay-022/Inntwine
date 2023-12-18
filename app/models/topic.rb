c4517d3c6a53a6201ac198c27a91acbde2f7f2cf
akshay-022 <62378087+akshay-022@users.noreply.github.com>
2023-12-18 14:52:46 -0500

    Full topic path now shows in post

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,19 @@ class Topic < ApplicationRecord
     belongs_to :parent, class_name: 'Topic', optional: true
     has_many :children, class_name: 'Topic', foreign_key: 'parent_id'
     has_many :posts
+
+    # Instance method to get the full pathname
+    def full_path_name
+        # Split the topic_path into individual topic IDs
+        path_ids = self.topic_path.split('/')
+
+        # Map the IDs to their corresponding topic names
+        path_names = path_ids.map do |id|
+        topic = Topic.find_by(id: id)
+        topic ? topic.topic_name : nil
+        end
+
+        # Join the names with slashes and return the full path name
+        path_names.compact.join('/')
+    end
 end
