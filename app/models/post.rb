d7c7858e78e46b8ce8b1095843d75f78d88e48f1
akshay-022 <62378087+akshay-022@users.noreply.github.com>
2024-01-02 08:01:40 +0530

    increased word limit

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   has_many :moderators
   belongs_to :organization
   has_and_belongs_to_many :topics
-  validates :body, length: { maximum: 600 }, allow_blank: false, unless: :post_id
+  validates :body, length: { maximum: 800 }, allow_blank: false, unless: :post_id
 
   def post_type
     if post_id? && body?
