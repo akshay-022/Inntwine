406f44d868e02784d72aa83f191fba848c562fa1
akshay-022 <62378087+akshay-022@users.noreply.github.com>
2024-01-02 17:49:57 +0530

    messages done perfectly maybe (except bug)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_02_092733) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_02_120644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -94,6 +94,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_02_092733) do
     t.integer "recipient_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "sender_unread"
+    t.boolean "recipient_unread"
   end
 
   create_table "datathings", force: :cascade do |t|
