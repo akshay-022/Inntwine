b3325e1b1763069ba4330267927b75f23556dd04
akshay-022 <62378087+akshay-022@users.noreply.github.com>
2024-01-12 16:24:02 +0530

    updated publicpost adding

--- db/migrate/20240112090106_add_all_posts_community_for_users.rb
+++ db/migrate/20240112090106_add_all_posts_community_for_users.rb
@@ -1,9 +1,14 @@
 class AddAllPostsCommunityForUsers < ActiveRecord::Migration[7.0]
-  def change
-    users_to_add = User.where.not(id: UserCommunity.where(organization_id: 2, topic_id: 0, part_of: true).pluck(:user_id))
+  def up
+    users_to_add = User.where.not(id: UserCommunity.where(organization_id: 2, topic_id: 0).pluck(:user_id))
     # Create UserCommunity records for the selected users
     users_to_add.each do |user|
       UserCommunity.create(user: user, organization_id: 2, topic_id: 0, part_of: true)
     end
+  end
+
+  def down
+    # Remove the UserCommunity records created in the 'up' method if necessary
+    UserCommunity.where(score: 0).destroy_all
   end
 end
